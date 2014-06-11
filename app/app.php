1061f54b4bd9a9e6d7d6473e88f35065a94e598e
vikekh <vieekk@gmail.com>
2014-06-11 22:09:53 +0200

    Working!

--- app/app.php
+++ app/app.php
@@ -4,9 +4,12 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/foo', function () {
+$app->get('/random', function () use ($app) {
+    $base = 'http://localhost/lampjavel-api/public/img/';
+    $files = scandir('img/');
+
     $app->response->setStatus(400);
-    $app->response->setBody('Foo');
+    $app->response->setBody($base . $files[mt_rand(2, count($files) - 1)]);
 });
 
 $app->run();
